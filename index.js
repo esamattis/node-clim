c3b096e98eabe4fd7946d31856a90ff8667c6df8
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-09-30 19:21:31 +0300

    Clean up

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
 
 var util = require("util");
+var cim;
 
-function cim(prefix, parent, patch) {
+module.exports = cim = function (prefix, parent, patch) {
   var ob;
+
+  // Fiddle optional arguments
   patch = Array.prototype.slice.call(arguments, -1)[0];
-
-  if (typeof patch !== "boolean") {
-    patch = false;
-  }
-
+  if (typeof patch !== "boolean") patch = false;
   if (typeof prefix === "object" && prefix !== null) {
     parent = prefix;
     prefix = undefined;
   }
 
-
-  // Patch given object if requested
   if (patch && parent) {
+    // Modify given object when patching is requested
     ob = parent;
-    ob._prefixes = ob._prefixes || [];
   }
-  // Otherwise create new object and inherit prefixes from parent
   else {
-    ob = { _prefixes: [] };
+    // Otherwise create new object
+    ob = {};
     if (parent && parent._prefixes) {
+      // and inherit prefixes from the given object
       ob._prefixes = parent._prefixes.slice();
     }
   }
 
-  consoleProxy(ob);
-
-  // Append new prefix if any
-  if (prefix) {
-    ob._prefixes.push(prefix);
-  }
+  // Append new prefix
+  if (!ob._prefixes) ob._prefixes = [];
+  if (prefix) ob._prefixes.push(prefix);
 
   ob.info = createLogger("INFO", ob._prefixes);
   ob.warn = createLogger("WARN", ob._prefixes);
   ob.error = createLogger("ERROR", ob._prefixes);
   ob.log = ob.info;
+  consoleProxy(ob);
+
   return ob;
-}
+};
 
 // By default write all logs to stderr
 cim.logWrite = function(method, msg){
@@ -54,8 +51,8 @@ cim.getTime = function(){
 
 
 // Just proxy methods we don't care about to original console object
-// Get method list from http://nodejs.org/api/stdio.html
 function consoleProxy(ob){
+  // list from http://nodejs.org/api/stdio.html
   var methods = ["dir", "time", "timeEnd", "trace", "assert"];
   methods.forEach(function(method){
     if (ob[method]) return;
@@ -67,15 +64,12 @@ function consoleProxy(ob){
 
 function createLogger(method, prefixes) {
   return function () {
+    // Handle formatting and circular objects like in the original
     var msg = util.format.apply(this, arguments);
-    var prefix = method + " " + prefixes.join(" ");
 
-    if (cim.getTime) {
-      prefix = cim.getTime() + " " + prefix;
-    }
+    var prefix = method + " " + prefixes.join(" ");
+    if (cim.getTime) prefix = cim.getTime() + " " + prefix;
 
     cim.logWrite(method, prefix.trim() + " " + msg);
   };
 }
-
-module.exports = cim;
