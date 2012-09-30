4415303f5c74cc2b8202d0f42dde3a9fdabd4f12
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-09-30 16:00:58 +0300

    Add example

---
+++ example/app.js
@@ -0,0 +1,24 @@
+
+express = require("express");
+cim = require("../index");
+
+var console = cim("APP");
+
+app = express();
+
+app.listen(8080, function(){
+  console.log("App is now listening on port 8080");
+});
+
+
+app.get("/", function(req, res) {
+  var console = cim("root", console);
+  console.log("Request");
+  res.send('<a href=/sub>sub<a/>');
+});
+
+app.get("/sub", function(req, res) {
+  var console = cim("sub", console);
+  console.log("Request");
+  res.send('<a href=/>root<a/>');
+});
